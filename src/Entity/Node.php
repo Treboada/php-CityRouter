919b961e3570a47abdaf89613bfd35f7525049eb
Rafa Couto <caligari@treboada.net>
2017-09-06 02:17:36 +0200

    Node and ServicePath entity members.

--- src/Entity/Node.php
+++ src/Entity/Node.php
@@ -1,7 +1,85 @@
 <?php
-
 namespace Treboada\CityRouter\Entity;
 
-class Node {
+class Node
+{
+
+    /**
+     *
+     * @var int
+     */
+    protected $id;
+
+    /**
+     *
+     * @var float
+     */
+    protected $latitude;
+
+    /**
+     *
+     * @var float
+     */
+    protected $longitude;
+
+    /**
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     *
+     * @param int $id
+     * @return Treboada\\CityRouter\\Entity\\Node
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     *
+     * @return float
+     */
+    public function getLatitude()
+    {
+        return $this->latitude;
+    }
+
+    /**
+     *
+     * @param float $latitude
+     * @return Treboada\\CityRouter\\Entity\\Node
+     */
+    public function setLatitude(float $latitude)
+    {
+        $this->latitude = $latitude;
+        return $this;
+    }
+
+    /**
+     *
+     * @return float
+     */
+    public function getLongitude()
+    {
+        return $this->longitude;
+    }
+
+    /**
+     *
+     * @param float $longitude
+     * @return Treboada\\CityRouter\\Entity\\Node
+     */
+    public function setLongitude(float $longitude)
+    {
+        $this->longitude = $longitude;
+        return $this;
+    }
 }
 
--- src/Entity/ServicePath.php
+++ src/Entity/ServicePath.php
@@ -1,7 +1,87 @@
 <?php
-
 namespace Treboada\CityRouter\Entity;
 
-class ServicePath {
+class ServicePath implements \ArrayAccess
+{
+
+    /**
+     *
+     * @var int
+     */
+    protected $id;
+
+    /**
+     *
+     * @var int[]
+     */
+    protected $nodes;
+
+    /**
+     * Contructor.
+     *
+     * @param array $initialData
+     */
+    public function __construct(array $initialData = array())
+    {
+        $this->nodes = $initialData;
+    }
+
+    /**
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     *
+     * {@inheritdoc}
+     * @see \\ArrayAccess::offsetExists()
+     */
+    public function offsetExists($offset)
+    {
+        return isset($this->nodes[$offset]);
+    }
+
+    /**
+     *
+     * {@inheritdoc}
+     * @see \\ArrayAccess::offsetGet()
+     */
+    public function offsetGet($offset)
+    {
+        return $this->nodes[$offset];
+    }
+
+    /**
+     *
+     * {@inheritdoc}
+     * @see \\ArrayAccess::offsetSet()
+     */
+    public function offsetSet($offset, $value)
+    {
+        $this->nodes[$offset] = $value;
+    }
+
+    /**
+     *
+     * {@inheritdoc}
+     * @see \\ArrayAccess::offsetUnset()
+     */
+    public function offsetUnset($offset)
+    {
+        unset($this->nodes[$offset]);
+    }
+
+    /**
+     *
+     * @return int[]
+     */
+    public function getArrayCopy()
+    {
+        return $this->nodes;
+    }
 }
 
